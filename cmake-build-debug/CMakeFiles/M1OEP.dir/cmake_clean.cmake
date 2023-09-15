a677e9f084932bd96629813be3029faf397dc1eb
Project1-wfoot <wfoot@uvm.edu>
2023-09-15 00:47:56 -0400

    Late: idk wtf is going on

---
+++ cmake-build-debug/CMakeFiles/M1OEP.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/M1OEP.dir/main.cpp.obj"
+  "CMakeFiles/M1OEP.dir/main.cpp.obj.d"
+  "M1OEP.exe"
+  "M1OEP.exe.manifest"
+  "M1OEP.pdb"
+  "libM1OEP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/M1OEP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
